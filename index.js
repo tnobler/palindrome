967f57868621097568f8d1a2257a5ec4b2241be9
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-10-12 16:17:11 -0500

    Initialize repository

---
+++ index.js
@@ -0,0 +1,37 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+  //Makes the phrase LOUDER.
+  this.louder = function louder() {
+    return this.content.toUpperCase();
+  };
+}
+
+// Definss a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
