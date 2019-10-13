48678241c3634072a7a834fcef8673ecb4cd2c51
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-10-13 14:15:50 -0500

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
@@ -9,10 +11,25 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
-  // Returns true for a palindrome, false otherwise.
+  // Returns the letters in the content.
+  // For example:
+  //    new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+    // let theLetters = [];
+    // const letterRegex = /[a-z]/i;
+    // Array.from(this.content).forEach(function(character) {
+    //   if (character.match(letterRegex)) {
+    //     theLetters.push(character);
+    //   }
+    // });
+    return theLetters.join("");
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Phrase object (with palindrome detector)
+This is a sample NPM module created in [*Learn Enough JavaScript to be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
+
+The module can be used as follows:
+
+```
+$ npm install --global mhartl-palindrome
+$ vim test.js
+let Phrase = require("mhartl-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+```
